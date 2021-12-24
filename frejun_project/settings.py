0b054b48738c240a536722024b72b4809f3561ee
Ram Babu <ram.kottapally@gmail.com>
2021-12-24 13:56:23 +0530

    Commented postgres db

--- frejun_project/settings.py
+++ frejun_project/settings.py
@@ -77,6 +77,14 @@ WSGI_APPLICATION = 'frejun_project.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+"""
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'frejun',
         'USER': 'postgres',
@@ -85,6 +93,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+"""
 
 
 REST_FRAMEWORK = {
